e543a6b413efd199079f9940b7bb3288084eb98c
jasonkroll <kroll@gmail.com>
2017-05-27 16:15:17 +1000

    Add mustachjs for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -9,27 +9,25 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('Received a new message', message)
-  var li = $('<li></li>');
   var formattedTime = moment(message.createdAt).format('h:mm a')
-  // es6 features won't work with IE. Need to use webpack
-  // li.text(`${message.from}: ${message.text}`);
-  li.text(message.from + ' ' + formattedTime + ': ' + message.text);
-
-  $('#messages').append(li);
+  var template = $('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
-  var li = $('<li></li>');
   var formattedTime = moment(message.createdAt).format('h:mm a')
-  
-  var a = $('<a target="_blank">My current location</a>');
-  li.text(message.from + ' ' + formattedTime + ': ' + message.text);
-
-  a.attr('href', message.url)
-  li.append(a);
-
-  $('#messages').append(li);
+  var template = $('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formattedTime,
+    url: message.url
+  })
+  $('#messages').append(html);
 });
 
 // Submit form for sending new message
